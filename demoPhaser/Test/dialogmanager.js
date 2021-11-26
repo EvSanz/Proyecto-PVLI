962e652843ca78af5975ca6d29c4da2af928bfc2
Bimbloc <rocisa08@ucm.es>
2021-11-26 23:01:15 +0100

    inicio de dialog manager
    a ver si conseguimos que esto funcione...

---
+++ demoPhaser/Test/dialogmanager.js
@@ -0,0 +1,20 @@
+export default class DialogManager extends Phaser.GameObjects.Sprite {
+
+    /**Gestioamos todos los dialogos del juego desde aqui
+    * @param {NPC[]} npc aqui se crean los npcs y se les asignan sus respectivos dialogos
+    * @param {Scene[]} dialog dialogo que se asignará a cada npc
+    */
+constructor(npc,dialog)
+ {
+     for( i=0;i<12;i++)
+     {
+
+
+     } 
+
+
+
+ }
+
+}
+    
