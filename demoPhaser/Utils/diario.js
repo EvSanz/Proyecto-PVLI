a4fa03d88a40327cab4cd867f97b7aa675729cc7
Javier V <javilleg@ucm.es>
2021-11-29 22:28:31 +0100

    Update diario.js
    ahora diario admite personajes y se ha documentado los metodos auxiliares

--- demoPhaser/Utils/diario.js
+++ demoPhaser/Utils/diario.js
@@ -1,11 +1,14 @@
 //Clase encargada de gestionar el diario
 export default class Diary extends Phaser.Scene {
-  constructor() { super({key: 'diary'});
+  constructor()
+  { 
+    super({key: 'diary'});
 
-      //Desactivamos los objetos que aparecen en el diario
-      this.tetera; 
+    this.objectList = [];
 
-      this.objectList = [];
+    this.characterList = [];
+
+    this.letterSize = 20;
   }
 
   //Cargamos la escena inicial
@@ -19,7 +22,7 @@ export default class Diary extends Phaser.Scene {
     this.add.sprite(500, 260, 'diary');
 
     //Por cada elemento del array de objetos se dibuja en el orden que se han obtenido
-    this.objectList.forEach(function a(element, index) {
+    this.objectList.forEach(function showObjects(element, index) {
 
       let yPos;
 
@@ -38,16 +41,41 @@ export default class Diary extends Phaser.Scene {
 
     }, this);
 
+
+    //Por cada personaje interrogado sale un poco de informacion de ellos
+    this.characterList.forEach(function showSuspects(element, index) {
+
+      //Mostramos el nombre...
+      this.add.text(540, 90 + index * this.letterSize * 1.8, element.name).setColor('black').setFontStyle('bold').setFontSize(this.letterSize);
+
+      //Y su coartada
+      this.add.text(555, 90 + this.letterSize + index * this.letterSize * 1.8, element.desc).setColor('black').setFontSize(this.letterSize - 5);
+
+    }, this);
+
     //Añadimos el comando para desactivar el diario 
     this.q = this.input.keyboard.addKey('Q'); 
-    this.q.on('down', cierraDiario => {
+    this.q.on('down', () => {
       this.scene.stop();
       this.scene.resume(this.currentScene);
     })
   }
-  
+
+  /** Agrega un objeto al array de objetos en el orden que se recogen
+  * @param {GameObject} obj Objeto a agregar
+  */
   addObject(obj)
   {
+    if (obj.sprite !== null)
       this.objectList.push(obj);
+  }
+
+  /** Agrega un personaje al array de personajes, pero primero se asegura que tiene los parametros necesarios
+  * @param {Struct} char Struct que contiene el nombre (name) y la coartada (desc)
+  */
+  addCharacter(char)
+  {
+    if (char.name !== null && char.desc !== null)
+      this.characterList.push(char);
   }
 }
