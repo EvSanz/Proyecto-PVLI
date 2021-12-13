78dc87b6405fc2727ee3c6bcccc36ae5125341ca
EvSanz <evsanc07@ucm.es>
2021-12-13 18:50:19 +0100

    Un poco de refactorizacion y he tocado wagon pq una variable no estaba bien escrita

--- demoPhaser/Wagons/clasemedia.js
+++ demoPhaser/Wagons/clasemedia.js
@@ -9,9 +9,9 @@ export default class ClaseMedia extends Wagon {
     }
     spawnNPCs()
     {
-      this.npc=new Npc(this,160,238,0,this.scene.get('boot').dmanager.npcinfoholder[2].anger,14,2);
-      this.npc2=new Npc(this,390,238,1,this.scene.get('boot').dmanager.npcinfoholder[1].anger,11,1);
-      this.npc3=new Npc(this,590,238,1,this.scene.get('boot').dmanager.npcinfoholder[6].anger,24,6);
+      this.npc=new Npc(this,160,238,3,this.scene.get('boot').dmanager.npcinfoholder[2].anger,14,22);
+      this.npc2=new Npc(this,390,238,4,this.scene.get('boot').dmanager.npcinfoholder[1].anger,11,5);
+      this.npc3=new Npc(this,590,238,5,this.scene.get('boot').dmanager.npcinfoholder[6].anger,24,8);
 
     }
     spawnObjects()
--- demoPhaser/Wagons/wagon.js
+++ demoPhaser/Wagons/wagon.js
@@ -42,7 +42,8 @@ export default class Wagon extends Phaser.Scene {
     //Creacion de la animacion del fondo
     this.dmanager = this.scene.get('boot').dmanager;
     this.gomanager = this.scene.get('boot').gomanager;
-    if(isPointAndClick)
+    
+    if(this.isPT)
     {
       this.musica = this.sound.add('musicafondopt', {volume: this.game.sound.volume * 0.5, loop: true});
     this.musica.play(); 
