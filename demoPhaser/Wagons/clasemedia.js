2710d0fe3055b8d38fe301b272a7b93b83ed643c
mcroche611 <mcroche@ucm.es>
2021-12-18 12:33:40 +0100

    Personajes recolocados

--- demoPhaser/Wagons/clasemedia.js
+++ demoPhaser/Wagons/clasemedia.js
@@ -15,15 +15,6 @@ export default class ClaseMedia extends Wagon {
     });
   }
   spawnNPCs() {
-    // this.npc = new Npc(this, 160, 238, 3, this.scene.get('boot').dmanager.npcinfoholder[2].anger,
-    //   14, this.scene.get('boot').dmanager.npcinfoholder[2].dialogo);
-
-    // this.npc2 = new Npc(this, 390, 238, 4, this.scene.get('boot').dmanager.npcinfoholder[3].anger,
-    //   11, this.scene.get('boot').dmanager.npcinfoholder[3].dialogo);
-
-    // this.npc3 = new Npc(this, 590, 238, 5, this.scene.get('boot').dmanager.npcinfoholder[4].anger,
-    //   24, this.scene.get('boot').dmanager.npcinfoholder[4].dialogo);
-
     this.addScenesNpc();
   }
 
@@ -32,7 +23,7 @@ export default class ClaseMedia extends Wagon {
     this.mortondoor = new Door(this, 120, 222, 'habitacionmorton', 'puertafun');
     this.mortondoor2 = new Door(this, 440, 222, 'habitacioncollins', 'puertafun');
     this.mortondoor3 = new Door(this, 780, 222, 'habitacionbold', 'puertafun');
-    this.phone = this.add.sprite(280, 230, 'objects', [23]).setDepth(1);
+    this.phone = this.add.sprite(560, 250, 'objects', [23]).setDepth(1);
     this.phone.setInteractive();
     this.phone.on('pointerdown', () => {
       this.scene.start('selectscene');
